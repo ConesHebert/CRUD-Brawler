64b54a96b38cc6f2257bd791fc2218292de5d997
Jones <161722209+ConesHebert@users.noreply.github.com>
2025-04-16 21:21:16 +0000

    Remoção de linhas ineficientes

--- listaBrawlers/CrudBrawlers/admin.py
+++ listaBrawlers/CrudBrawlers/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Brawlers
 
+
+
 @admin.register(Brawlers)
 class listaBrawler(admin.ModelAdmin):
     list_display = ("Nome",'starPowers','gadgets','raridade')
     list_filter = ['raridade']
+
     
+    # Tirar o registro
+    # admin.site.unregister(Brawlers)
+
 
--- listaBrawlers/CrudBrawlers/models.py
+++ listaBrawlers/CrudBrawlers/models.py
@@ -2,19 +2,18 @@ from django.db import models
 
 
     # CHOICES de raridade
+
 RARIDADE_CHOICES = [
-        ('RR', "Raro"),
-        ('SR', "Super Raro"),
-        ('EP', "Épico"),
-        ('MT', "Mítico"),
-        ('LD', "Lendario"),
+    ('RR', "Raro"),
+    ('SR', "Super Raro"),
+    ('EP', "Épico"),
+    ('MT', "Mítico"),
+    ('LD', "Lendario"),
 ]
 
 class Brawlers(models.Model):
     Nome = models.CharField(max_length=100, verbose_name='Nome')
     starPowers = models.CharField(max_length=100, verbose_name='Star Powers')
     gadgets = models.CharField(max_length=100, verbose_name='Gadgets')
-
-    raridade = models.CharField(max_length=2, choices=RARIDADE_CHOICES, verbose_name='Raridade')
-
+    raridade = models.CharField(max_length=100, verbose_name='Raridade', choices=RARIDADE_CHOICES)
 
