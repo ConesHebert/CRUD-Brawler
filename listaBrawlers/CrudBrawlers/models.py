ce95f8ad725bba6ea0883842230b3c5aa6c36ec8
Jones <161722209+ConesHebert@users.noreply.github.com>
2025-04-16 19:54:06 +0000

    Alteração no models e adição do login do superuser

--- listaBrawlers/CrudBrawlers/models.py
+++ listaBrawlers/CrudBrawlers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+    # CHOICES de raridade
+RARIDADE_CHOICES = [
+        ('RR', "Raro"),
+        ('SR', "Super Raro"),
+        ('EP', "Épico"),
+        ('MT', "Mítico"),
+        ('LD', "Lendario"),
+]
+
+class Brawlers(models.Model):
+    Nome = models.CharField(max_length=100, verbose_name='Nome')
+    starPowers = models.CharField(max_length=100, verbose_name='Star Powers')
+    gadgets = models.CharField(max_length=100, verbose_name='Gadgets')
+
+    raridade = models.CharField(max_length=2, choices=RARIDADE_CHOICES, verbose_name='Raridade')
